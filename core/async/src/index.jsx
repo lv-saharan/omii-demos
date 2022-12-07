2498197166fb065265eca4a279a60167bb11130f
lv-saharan <saharan@foxmail.com>
2022-12-07 18:57:54 +0800

    await import use promise to avoid block

---
+++ core/async/src/index.jsx
@@ -0,0 +1,33 @@
+import { h, render, define, Component } from "omii";
+
+import css from "./index.scss";
+
+const sleep = n => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, n * 1000);
+  })
+}
+define("oi-async-1", class extends Component {
+
+  async render() {
+    await sleep(1)
+    return <>
+      <h1>asyc....</h1>
+    </>
+  }
+})
+
+define("oi-app", class extends Component {
+
+  render() {
+    return <>
+      <h1>hello,world</h1>
+      <oi-async-1 />
+    </>
+  }
+})
+
+
+render(<oi-app></oi-app>, 'body')
